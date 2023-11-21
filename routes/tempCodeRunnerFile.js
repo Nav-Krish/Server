6f7d611b7fb6a877962c9ed560787bc47b94e63e
Nav-Krish <navanethakrishnan11@gmail.com>
2023-11-21 21:46:19 +0530

    commit

---
+++ routes/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+try {
+    
+  } catch (error) {
+    res.send("Not Verified");
+    console.log(error);
+  }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,8 +8,17 @@ const {
 const { User, generateToken } = require("../models/userModel.js");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
+const expressLayouts = require('express-ejs-layouts');
 
+const app = express()
 const router = express.Router();
+
+var ejs = require('ejs');
+
+// Ejs 
+app.set('view engine', 'ejs')
+app.use(expressLayouts);
+
 
 // login - already existing users
 router.post("/login", async (req, res) => {
@@ -71,8 +80,8 @@ router.post("/forgot-password", async (req, res) => {
     }
     // To generate a random string consist of some token and secret key
     const secret = "secret_key" + user.password;
-    const token = jwt.sign({ email: user.email, id: user._id }, secret, {
-      expiresIn: "5m",
+    const token = jwt.sign({ email: user.email }, secret, {
+      expiresIn: "15m"
     });
 
     // password reset link to be sent to the user via email
@@ -101,7 +110,7 @@ router.post("/forgot-password", async (req, res) => {
         console.log("Email sent: " + info.response);
       }
     });
-    res.status(200).json({ message: "Password reset link sent.", token });
+    res.status(200).json({ message: "Password reset link sent.", token, secret });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Internal Server Error." });
@@ -117,40 +126,43 @@ router.get("/reset-password/:id/:token", async (req, res) => {
     return res.status(404).json({ error: "User does not exists." });
   }
   // To verify the random string sent via email using jwt
-  const secret = "secret_key"+ user.password;
+  const secret = "secret_key" + user.password;
+  
   try {
     const verify = jwt.verify(token, secret);
-    console.log(verify);
+    console.log(verify.email);
     // To load the html file where the form is displayed to enter the new password
-    res.render("index", { email: verify.email, status: "Not Verified" });
+    res.render("./index.ejs", { email: verify.email, status: "Not Verified" });
   } catch (error) {
     res.send("Not Verified");
     console.log(error);
-  }
+  } 
 });
 
 // post reset-password routes
 router.post("/reset-password/:id/:token", async (req, res) => {
-    const { id, token } = req.params;
-    const { password } = req.body;
-    // // To check whether the user exists in db or not
-    const user = await User.findOne({ _id: id });
-    if (!user) {
-      return res.status(404).json({ error: "User does not exists." });
-    }
-    const secret = "secret_key" + user.password;
-    try {
-      const verify = jwt.verify(token, secret);
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      await User.updateOne({ _id: id }, { $set: { password: hashedPassword } });
-      // This load the html file where the form is displayed to enter new password
-      res.render("index", { email: verify.email, status: "verified" });
-    } catch (error) {
-      res.json({ status: "Something went wrong" });
-      console.log(error);
-    }
-  });
+  const { id, token } = req.params;
+  const { password } = req.body;
+  // checking whether the new password and confirm password is same
+  // check user is exits
+  const user = await User.findOne({ _id: id });
+  if (!user) {
+    return res.status(404).json({ error: "User does not exists." });
+  }
+  const secret = "secret_key" + user.password;
+  try {
+    const verify = jwt.verify(token, secret);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    await User.updateOne({ _id: id }, { $set: { password: hashedPassword } });
+    // res.json({ status: "Password Updated" });
+    // This load the html file where the form is displayed to enter new password
+    res.render("./index.ejs", { email: verify.email, status: "verified" });
+  } catch (error) {
+    res.json({ status: "Something went wrong" });
+    console.log(error);
+  }
+});
 
 //home
 router.get("/", async(req, res) => {
